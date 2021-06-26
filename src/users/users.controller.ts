9d3c03ada243690220291d44e74640a99b16e77e
oohyun15 <sakiss4774@gmail.com>
2021-06-26 20:15:00 +0900

    Check whether user exists

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post } from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { User } from "./user.entity";
 import { UsersService } from "./users.service";
@@ -19,15 +19,20 @@ export class UsersController {
 
   @Get(':id')
   findOne(@Param('id') id: string): Promise<User> {
-    return this.usersService.findOne(id);
+    return this.getUser(id);
   }
 
   @Delete(':id')
   async remove(@Param('id') id: string): Promise<string> {
-    const user = await this.usersService.findOne(id);
-    if (user === undefined) return `There is no user. (id: ${id})`;
+    const user = await this.getUser(id);
     this.usersService.remove(id);
     const fullName = user.firstName + ' ' + user.lastName
     return `User: ${fullName} is deleted.`;
+  }
+
+  private async getUser(id: string) {
+    const user = await this.usersService.findOne(id);
+    if (user === undefined) { throw new HttpException(`There is no user. (id: ${id})`, HttpStatus.NOT_FOUND); }
+    return user;
   }
 }
