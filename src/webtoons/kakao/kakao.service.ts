462fc4b78b354297cb71ef1b5f4d7fca4665c48f
oohyun15 <sakiss4774@gmail.com>
2022-01-17 00:47:52 +0900

    :sparkles: Extract all identifier from kakao webtoon

--- src/webtoons/kakao/kakao.service.ts
+++ src/webtoons/kakao/kakao.service.ts
@@ -4,6 +4,19 @@ import { DeleteResult, Repository } from 'typeorm';
 import { CreateWebtoonDto } from '../dto/create-webtoon.dto';
 import { Kakao } from './kakao.entity';
 
+const generalNewestLink: string =
+  'https://gateway-kw.kakao.com/section/v1/sections?placement=channel_new';
+const generalWeekdayLink: string =
+  'https://gateway-kw.kakao.com/section/v1/pages/general-weekdays';
+const generalCompletedLink: string =
+  'https://gateway-kw.kakao.com/section/v1/sections?placement=channel_completed';
+const novelNewestLink: string =
+  'https://gateway-kw.kakao.com/section/v1/sections?placement=novel_new';
+const novelWeekdayLink: string =
+  'https://gateway-kw.kakao.com/section/v1/pages/novel-weekdays';
+const novelCompletedLink: string =
+  'https://gateway-kw.kakao.com/section/v1/sections?placement=novel_completed';
+
 @Injectable()
 export class KakaoService {
   constructor(
@@ -40,6 +53,45 @@ export class KakaoService {
     return result;
   }
 
+  async extractAllIdentifiers() {
+    const links: string[] = [
+      generalNewestLink,
+      generalWeekdayLink,
+      generalCompletedLink,
+      novelNewestLink,
+      novelWeekdayLink,
+      novelCompletedLink,
+    ];
+    links.forEach((link) => {
+      this.extractIdentifier(link);
+    });
+  }
+
+  async extractIdentifier(link: string) {
+    let count = 0;
+
+    const resp = await this.http.get(link).toPromise();
+    let list;
+    if (resp.data.data.sections !== undefined) {
+      // hash type
+      list = resp.data.data.sections.map((s) => s.cardGroups[0].cards).flat();
+    } else if (resp.data.data[0] !== undefined) {
+      // array type
+      list = resp.data.data[0].cardGroups[0].cards;
+    }
+    // return list;
+    const identifiers: string[] = list
+      .filter((e) => e.content.ageLimit != 19)
+      .map((e) => e.content.id.toString());
+
+    identifiers.forEach((identifier) => {
+      let dto = new CreateWebtoonDto();
+      dto.type = 'Kakao';
+      dto.identifier = identifier;
+      this.create(dto);
+    });
+  }
+
   async crawl(id: number) {
     // find webtoon
     let webtoon = await this.kakaoRepository.findOne(id);
@@ -58,6 +110,8 @@ export class KakaoService {
     // save webtoon
     return this.kakaoRepository.save(webtoon);
   }
+
+  // private
 
   private sanitizeStatus(status: string): number {
     switch (status) {
