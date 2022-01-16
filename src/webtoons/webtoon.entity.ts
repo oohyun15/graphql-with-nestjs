04e607212c7433e081f6c76e708ef23b20800e54
oohyun15 <sakiss4774@gmail.com>
2022-01-16 18:59:48 +0900

    :wrench: Edit webtoon entity

--- src/webtoons/webtoon.entity.ts
+++ src/webtoons/webtoon.entity.ts
@@ -4,28 +4,51 @@ import {
   PrimaryGeneratedColumn,
   TableInheritance,
   Index,
+  BeforeUpdate,
 } from 'typeorm';
 
 @Entity()
 @TableInheritance({ column: { type: 'varchar', name: 'type' } })
 @Index(['type', 'identifier'], { unique: true })
-@Index(['internalId', 'internalType'])
 export class Webtoon {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ nullable: true })
-  internalId: number;
-
-  @Column({ nullable: true, length: 100 })
-  internalType: string;
-
   @Column()
   identifier: string;
+
+  @Column()
+  title: string;
+
+  @Column({ nullable: true, type: 'text' })
+  description: string;
+
+  @Column({ nullable: true })
+  status: number;
+
+  @Column({ nullable: true })
+  gradeAge: number;
+
+  @Column({ nullable: true })
+  weekDay: number;
+
+  @Column({ nullable: true, type: 'timestamp' })
+  startDate: Date;
+
+  @Column({ nullable: true, type: 'timestamp' })
+  endDate: Date;
 
   @Column({ nullable: true, type: 'text' })
   data: string;
 
-  @Column({ nullable: true })
-  status: number;
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  updated: Date;
+
+  @BeforeUpdate()
+  updateTimestamp() {
+    this.updated = new Date();
+  }
 }
