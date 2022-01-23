40e28471d1ffe8fa2c8ab2efa00ab75090bbeb29
oohyun15 <sakiss4774@gmail.com>
2022-01-23 21:35:50 +0900

    :tada: Initialize webtoon resolver

--- src/webtoons/webtoons.module.ts
+++ src/webtoons/webtoons.module.ts
@@ -4,9 +4,10 @@ import { WebtoonsService } from './webtoons.service';
 import { KakaoService } from './kakao/kakao.service';
 import { Webtoon } from './webtoon.entity';
 import { Kakao } from './kakao/kakao.entity';
+import { WebtoonsResolver } from './webtoons.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Webtoon, Kakao]), HttpModule],
-  providers: [WebtoonsService, KakaoService],
+  providers: [WebtoonsService, KakaoService, WebtoonsResolver],
 })
 export class WebtoonsModule {}
